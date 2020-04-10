fa6d315aed8011adf979eb9d4a05ac40d7bc148b
Relag <44652125+Relag@users.noreply.github.com>
2020-04-10 18:19:45 -0400

    Set Health in Constructor
    Forgot to set health to maxhealth in the constructors for Goblin and Hero.

--- testing/Battle System Test/ConsoleApplication1/Hero.cpp
+++ testing/Battle System Test/ConsoleApplication1/Hero.cpp
@@ -7,6 +7,8 @@ Hero::Hero() {
 	m_Stats.m_Agility = 2 + (rand() % 3);
 	m_Stats.m_MaxHealth = 12 + (rand() % 6);
 	m_Stats.m_Strength = 4 + (rand() % 2);
+	m_Stats.m_Health = m_Stats.m_MaxHealth;
+
 }
 
 void Hero::heal(Hero* ally) {
