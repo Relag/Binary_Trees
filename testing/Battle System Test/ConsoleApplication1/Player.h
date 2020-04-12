95676327db66a701eecd231a316654ea2b02de1f
Relag <44652125+Relag@users.noreply.github.com>
2020-04-12 17:34:36 -0400

    Update Player.h
    Outlined functions for Player class. Act, constructor and destructor.

---
+++ testing/Battle System Test/ConsoleApplication1/Player.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Fighter.h"
+
+class Player :
+	public Fighter
+{
+public:
+	Player();
+	~Player();
+	void Act(std::vector<Fighter*> allies, std::vector<Fighter*> enemies);
+};
+
