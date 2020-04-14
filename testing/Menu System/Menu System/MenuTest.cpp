76706604a2e39b5a4c2ff3725bf4da1cf364fa81
Relag <44652125+Relag@users.noreply.github.com>
2020-04-13 23:20:14 -0400

    Update MenuTest.cpp
    Can now navigate between menu options with up and down arrows.

--- testing/Menu System/Menu System/MenuTest.cpp
+++ testing/Menu System/Menu System/MenuTest.cpp
@@ -1,16 +1,27 @@
 #include<iostream>
 #include<vector>
+#include<stdio.h>
+#include<Windows.h>
 
 //May create as a singleton later.
 //Pass in pre-created iterator and have a separate switch statement to interpret the choice?
 void Menu(std::vector<std::string> choices, std::string intro = "") {
 	std::cout << std::endl << intro << std::endl << std::endl;
 	std::vector<std::string>::iterator iter = choices.begin();
-	for (int i = 0; i < choices.size(); i++) {
-		if ((*iter) == choices[i]) {
-			std::cout << "-->";
+	int cDirection = 0;
+	while (!GetKeyState(VK_RETURN)){
+		for (int i = 0; i < choices.size(); i++) {
+			if ((*iter) == choices[i]) {
+				std::cout << "-->";
+			}
+			std::cout << "\t" << choices[i] << std::endl;
 		}
-		std::cout << "\t" << choices[i] << std::endl;
+		Sleep(100);
+		if (GetKeyState(VK_DOWN) & 0x8000)
+			iter++;
+		else if (GetKeyState(VK_UP) & 0x8000)
+			iter--;
+		system("CLS");
 	}
 }
 
