7d7cc70e86550f4149f982203e0fe597564eeaa7
Relag <44652125+Relag@users.noreply.github.com>
2020-04-13 23:27:26 -0400

    Update MenuTest.cpp
    Added security to ensure the iterator does not escape the bounds of the vector.

--- testing/Menu System/Menu System/MenuTest.cpp
+++ testing/Menu System/Menu System/MenuTest.cpp
@@ -6,7 +6,7 @@
 //May create as a singleton later.
 //Pass in pre-created iterator and have a separate switch statement to interpret the choice?
 void Menu(std::vector<std::string> choices, std::string intro = "") {
-	std::cout << std::endl << intro << std::endl << std::endl;
+	std::cout << intro;
 	std::vector<std::string>::iterator iter = choices.begin();
 	int cDirection = 0;
 	while (!GetKeyState(VK_RETURN)){
@@ -16,10 +16,10 @@ void Menu(std::vector<std::string> choices, std::string intro = "") {
 			}
 			std::cout << "\t" << choices[i] << std::endl;
 		}
-		Sleep(100);
-		if (GetKeyState(VK_DOWN) & 0x8000)
+		Sleep(50);
+		if (GetKeyState(VK_DOWN) & 0x8000 && iter != choices.end() - 1)
 			iter++;
-		else if (GetKeyState(VK_UP) & 0x8000)
+		else if (GetKeyState(VK_UP) & 0x8000 && iter != choices.begin())
 			iter--;
 		system("CLS");
 	}
