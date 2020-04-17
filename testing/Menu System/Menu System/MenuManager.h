c58be91cc874464228d51a3064feb082796b8c5f
Relag <44652125+Relag@users.noreply.github.com>
2020-04-17 01:23:04 -0400

    Created MenuManager
    Created the MenuManager singleton. This will house the Menu() function in the main game.

---
+++ testing/Menu System/Menu System/MenuManager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<iostream>
+#include<vector>
+#include<stdio.h>
+#include<conio.h>
+#include<Windows.h>
+class MenuManager
+{
+private:
+	static MenuManager* instance;
+	MenuManager();
+public:
+	//void CreateMenu(std::vector<T> choices, std::string intro = "")
+};
+
